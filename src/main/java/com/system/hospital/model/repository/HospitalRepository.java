7938bd20a767cc351d5475e54983af8eba768ba7
Kawan Ximendes <107648914+KawanXLima@users.noreply.github.com>
2024-09-27 23:48:01 -0300

    Revert "Feat/relatorios"

--- src/main/java/com/system/hospital/model/repository/HospitalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.system.hospital.model.repository;
-
-import com.system.hospital.model.entity.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface HospitalRepository extends JpaRepository<Hospital,Integer>   {
-}
