c10dcd403c42f105b83a3386882d6c7947f3898b
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-09-27 23:25:54 -0300

    feat: created entity relatorio

---
+++ src/main/java/com/system/hospital/model/repository/RelatorioRepository.java
@@ -0,0 +1,8 @@
+package com.system.hospital.model.repository;
+
+import com.system.hospital.model.entity.Relatorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RelatorioRepository extends JpaRepository<Relatorio, Integer> {
+
+}
